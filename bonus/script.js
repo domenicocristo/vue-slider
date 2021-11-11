1875063e2f90ef8c81dea0e8ed751f19a1523420
MyUsername <my@email.com>
2021-11-11 16:51:34 +0100

    bonus

---
+++ bonus/script.js
@@ -0,0 +1,53 @@
+var  carousel = new Vue({
+    el: "#container",
+    data: {
+        activeSlide: 0,
+        slides: [
+            {
+                image: "img/01.jpg",
+                title: "Svezia",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+            },
+
+            {
+                image: "img/02.jpg",
+                title: "Svizzera",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+            },
+
+            {
+                image: "img/03.jpg",
+                title: "Gran Bretagna",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+            },
+
+            {
+                image: "img/04.jpg",
+                title: "Germania",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+            },
+
+            {
+                image: "img/05.jpg",
+                title: "Paradine",
+                text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+            },
+        ],
+    },
+    methods: {
+        nextSlide: function() {
+            if (this.activeSlide === this.slides.lenght - 1) {
+                this.activeSlide = 0;
+            } else {
+                this.activeSlide ++;
+            }
+        },
+        prevSlide: function() {
+            if (this.activeSlide === 0) {
+                this.activeSlide = this.slides.lenght - 1;
+            } else {
+                this.activeSlide --;
+            }
+        }
+    }
+});
